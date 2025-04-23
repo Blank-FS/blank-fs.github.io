4416f72a7407ff10088aab1f7b294dfd2c7d0e15
Felix Shen <shen68601@gmail.com>
2025-04-23 00:17:41 -0400

    Updated personal site

--- data/content.ts
+++ data/content.ts
@@ -22,9 +22,9 @@ export const about: AboutType = {
   paragraphs: [
     "I'm a rising junior at the University of Michigan pursuing a BSE in Computer Science, currently holding a 4.0 unweighted GPA. My academic journey is driven by a deep passion for technology and a commitment to continuous learning and growth.",
 
-    "I’ve had the opportunity to gain hands-on experience through internships and personal projects, where I’ve worked across the full stack using modern frameworks and tools like Next.js, Spring Boot, and both SQL and NoSQL databases. During my internship at Shenzhen Ningyuan Technology Co., I streamlined API documentation processes by transitioning from a static frontend to a dynamic backend solution using Spring Boot and MySQL — boosting productivity and maintainability for the development team.",
+    "I've had the opportunity to gain hands-on experience through internships and personal projects, where I've worked across the full stack using modern frameworks and tools like Next.js, Spring Boot, and both SQL and NoSQL databases. During my internship at Shenzhen Ningyuan Technology Co., I streamlined API documentation processes by transitioning from a static frontend to a dynamic backend solution using Spring Boot and MySQL — boosting productivity and maintainability for the development team.",
 
-    "Outside of internships, I’ve worked on several projects such as TeamUp, a full-stack matchmaking platform for hackathons built using Next.js and MongoDB, and KotobaQuest, a Japanese vocabulary learning tool that’s actively helped students at my high school. Currently, I'm building an over-the-top (OTT) platform with e-commerce integration using Next.js for frontend and Spring Boot for backend. These experiences not only sharpened my development skills but also taught me the value of collaboration, design thinking, and creating meaningful user experiences.",
+    "Outside of internships, I've worked on several projects such as TeamUp, a full-stack matchmaking platform for hackathons built using Next.js and MongoDB, and KotobaQuest, a Japanese vocabulary learning tool that's actively helped students at my high school. Currently, I'm building an over-the-top (OTT) platform with e-commerce integration using Next.js for frontend and Spring Boot for backend. These experiences not only sharpened my development skills but also taught me the value of collaboration, design thinking, and creating meaningful user experiences.",
 
     "I'm especially enthusiastic about exploring opportunities where I can grow as a developer, contribute to impactful projects, and work with innovative teams. Whether it's building scalable backend systems, crafting intuitive frontends, or contributing to open-source, I'm eager to keep pushing boundaries and learning more every day.",
   ],
@@ -36,7 +36,7 @@ export const education: EducationType = {
   imageAlt: "Michigan Engineering Logo",
   date: "Aug 2024 - May 2027",
   location: "Ann Arbor, MI, United States",
-  overview: "BSE in Computer Science (Rising Junior) - GPA: 4.0",
+  overview: "BSE in Computer Science (Junior) - GPA: 4.0",
   courseLink: "https://bulletin.engin.umich.edu/courses/eecs/",
   courseworks: [
     "EECS 489 (Computer Networks)",
@@ -53,13 +53,27 @@ export const education: EducationType = {
 
 export const experiences: ExperienceType[] = [
   {
-    title: "IGMR Lab Research Assistant",
+    title: "IGMR Lab Researcher",
     date: "Apr 2025 - Present",
     organization: "University of Michigan Robotics Department",
     location: "Ann Arbor, MI, United States",
     image: "/experience/umichRob.png",
     imageAlt: "Univeristy of Michigan Robotics Department Logo",
-    points: ["Summer Undergraduate Research in Engineering - SURE 2025"],
+    points: [
+      "Summer Undergraduate Research in Engineering - SURE 2025",
+      "Develop GUI for advanced robotic microsurgical system with real-time OCT imaging and 3D visualization",
+    ],
+  },
+  {
+    title: "Research Programmer",
+    date: "Apr 2025 - Present",
+    organization: "Michigan Medicine",
+    location: "Ann Arbor, MI, United States",
+    image: "/experience/umichMed.png",
+    imageAlt: "Michigan Medicine Logo",
+    points: [
+      "Create dynamically generated interactive icon arrays for research study on positively addressing vaccine hesitancy",
+    ],
   },
   {
     title: "Full-stack Developer Intern",
@@ -70,13 +84,13 @@ export const experiences: ExperienceType[] = [
     imageAlt: "Health 160 Logo",
     points: [
       "Employed Vue.js for frontend, Spring Boot for backend, and MySQL for database",
-      "Migrated the company’s API documentation from a static front-end implementation to a dynamic back-end solution, enabling the storage and management of documentation within a centralized database",
+      "Migrated the company's API documentation from a static front-end implementation to a dynamic back-end solution, enabling the storage and management of documentation within a centralized database",
       "Enhanced the efficiency of the development team by providing a more agile and user-friendly system for managing API documentation, thereby boosting overall productivity",
     ],
   },
   {
     title: "Store Associate",
-    date: "May 2024 – Jun 2024",
+    date: "May 2024 - Jun 2024",
     organization: "Shoppers Drug Mart",
     location: "Burnaby, BC, Canada",
     image: "/experience/shoppers.png",
@@ -88,7 +102,7 @@ export const experiences: ExperienceType[] = [
   },
   {
     title: "Technician Assistant",
-    date: "July 2023 – Aug 2023",
+    date: "July 2023 - Aug 2023",
     organization: "BC Technology for Learning Society",
     location: "Burnaby, BC, Canada",
     image: "/experience/bcTech.png",
@@ -102,31 +116,58 @@ export const experiences: ExperienceType[] = [
 
 export const projects: ProjectType[] = [
   {
+    title: "Ineed.io",
+    date: "Jan 2025 - Apr 2025",
+    stack: [
+      "TypeScript",
+      "React.js",
+      "Next.js",
+      "Supabase",
+      "Figma",
+      "Tailwind CSS",
+      "GitHub",
+    ],
+    link: "https://github.com/Blank-FS/michigan-hackers-ineedio-web",
+    points: [
+      "Michigan Hackers advanced web dev team project- internship tracker platform for students to manage applications",
+      "Performed code auditing, refactored code base, and caught infinite api request bug in useEffect dependency array",
+      "Utilized Tailwind CSS, shadcn/ui, and Figma for a modern, intuitive UI/UX design",
+    ],
+  },
+  {
     title: "TeamUp",
     date: "Sept 2024",
-    stack: ["Next.js", "Prisma", "MongoDB", "GitHub", "Auth0", "Faker.js"],
-    link: "https://github.com/Blank-FS/team-up",
+    stack: [
+      "TypeScript",
+      "React.js",
+      "Next.js",
+      "Prisma",
+      "MongoDB",
+      "GitHub",
+      "Auth0",
+      "Faker.js",
+    ],
+    link: "https://github.com/Blank-FS/mhacks-team-up",
     points: [
-      "MHacks-2024 (Hackathon) project. The website helps hackers form teams by matching them with other users with similar skill sets/tech stacks",
-      "Worked full-stack on the project and collaborated with others using GitHub version controls",
-      "Set up the Next.js application, Auth0 authentication, and the Prisma ORM which connects to the database on MongoDB Atlas",
-      "Took part in designing the database schemas, writing APIs that perform CRUD operations, generating user data using faker.js for testing purposes, and the design and implementation of the interactive UI for the website",
+      "MHacks 2024 (Hackathon) project- a matchmaking platform to help hackers form teams at hackathons",
+      "Worked as team lead on the full-stack website and collaborated with others using GitHub",
+      "Set up Next.js application, Auth0 authentication, and Prisma ORM with MongoDB atlas",
+      "Designed database schemas, wrote APIs that perform CRUD operations, generated mock user data using faker.js, and created interactive UI for the website",
     ],
   },
   {
     title: "API Documentation Backend",
-    date: "July 2024 – Aug. 2024",
+    date: "July 2024 - Aug 2024",
     stack: ["Java", "Maven", "Spring Boot", "JPA/Hibernate", "MySQL", "Git"],
     link: "https://github.com/Blank-FS/api-document",
     points: [
-      "Developed a Spring Boot backend that stores API documents organized by category and topic as String data in a relational database",
-      "Used Spring Boot JPA/Hibernate for ORM with a MySQL database",
-      "Implemented CRUD operations allowing for the modification of data in the database and the sending of content to the frontend",
+      "Developed a Spring Boot backend that manages markdown api documentation in database",
+      "Used JPA/Hibernate for ORM and implemened CRUD operations for database entry modifications",
     ],
   },
   {
     title: "KotobaQuest",
-    date: "Mar. 2024 – Apr. 2024",
+    date: "Mar 2024 - Apr 2024",
     stack: [
       "Next.js",
       "Prisma",
